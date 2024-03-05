8a8a77d99cda28cc76415e2b394eb9ce9ca6a713
Joel Plummer <joecodescs@gmail.com>
2024-03-05 00:48:09 -0500

    `` Exercise 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from .config import Config
+from flask_migrate import Migrate;
 # import flask migrate here
 
 app = Flask(__name__)
@@ -9,10 +10,14 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 # Instantiate Flask-Migrate library here
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+
+
 
 from app import views
